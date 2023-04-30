73f8f16c238d647d60c7c4f44a82356e6d259e6d
William Wall <will.wall@tealhq.com>
2023-04-29 21:22:47 -0600

    move treesitter config out to own file
    Also disable treesitter for large files

--- init.lua
+++ init.lua
@@ -19,25 +19,6 @@ return require('packer').startup(function(use)
         ts_update()
     end,
   }
-  require('nvim-treesitter.configs').setup {
-    -- Automatically install missing parsers when entering buffer
-    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-    auto_install = true,
-
-    highlight = {
-      enable = true,
-
-      -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-      -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-      -- Using this option may slow down your editor, and you may see some duplicate highlights.
-      -- Instead of true it can also be a list of languages
-      additional_vim_regex_highlighting = false,
-    },
-
-    indent = {
-      enable = true,
-    },
-  }
 
   use 'terrortylor/nvim-comment'
   require('nvim_comment').setup()
@@ -58,5 +39,6 @@ return require('packer').startup(function(use)
   require('colors')
   require('lsp')
   require('telescope-config')
+  require('treesitter-config')
   require('formatter-config')
 end)
