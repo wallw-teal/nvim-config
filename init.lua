1a46e410831eb51060a73436777450f0e80e949c
William Wall <will.wall@tealhq.com>
2023-04-29 21:16:23 -0600

    use tree-sitter for indent

--- init.lua
+++ init.lua
@@ -31,7 +31,11 @@ return require('packer').startup(function(use)
       -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
       -- Using this option may slow down your editor, and you may see some duplicate highlights.
       -- Instead of true it can also be a list of languages
-      additional_vim_regex_highlighting = true,
+      additional_vim_regex_highlighting = false,
+    },
+
+    indent = {
+      enable = true,
     },
   }
 
