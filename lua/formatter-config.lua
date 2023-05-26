a482b50beca41f55690e108b9456a0c02acd4234
William Wall <will.wall@tealhq.com>
2023-05-26 12:17:26 -0600

    add shortcuts for format and auto format on save

--- lua/formatter-config.lua
+++ lua/formatter-config.lua
@@ -2,50 +2,62 @@ vim.cmd([[nnoremap <silent> <leader>f :Format<CR>]])
 
 require('formatter').setup({
   filetype = {
-    typescript = { 
-      function() 
+    typescript = {
+      function()
         return {
           exe = "prettier",
           args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)), '--single-quote'},
           stdin = true
         }
-      end 
+      end
     },
-    typescriptreact = { 
-      function() 
+    typescriptreact = {
+      function()
         return {
           exe = "prettier",
           args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)), '--single-quote'},
           stdin = true
         }
-      end 
+      end
     },
-    javascript = { 
-      function() 
+    javascript = {
+      function()
         return {
           exe = "prettier",
           args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)), '--single-quote'},
           stdin = true
         }
-      end 
+      end
     },
-    javascriptreact = { 
-      function() 
+    javascriptreact = {
+      function()
         return {
           exe = "prettier",
           args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)), '--single-quote'},
           stdin = true
         }
-      end 
+      end
     },
-    json = { 
-      function() 
+    json = {
+      function()
         return {
           exe = "prettier",
           args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)), '--single-quote'},
           stdin = true
         }
-      end 
+      end
     }
   }
 })
+
+-- Assign formatting shortcuts for formatting (aka "pretty")
+vim.api.nvim_set_keymap('n', '<leader>p', ':Format<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>P', ':FormatWrite<CR>', { noremap = true })
+
+-- Auto format on save
+vim.cmd [[
+  augroup FormatAutoGroup
+    autocmd!
+    autocmd BufWritePost * FormatWrite
+  augroup END
+]]
