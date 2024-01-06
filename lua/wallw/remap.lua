b304ebfd04a46b273fe1e4adefee57f5951e587b
William Wall <will.wall@tealhq.com>
2024-01-05 23:36:31 -0700

    change key binding for netrw

--- lua/wallw/remap.lua
+++ lua/wallw/remap.lua
@@ -1,5 +1,7 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- launch netrw file browser
+vim.keymap.set("n", "<leader>fv", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
